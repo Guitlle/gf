942533781a9d1da3fc22e4d84b7100d9b5776ff5
Guillermo <lgar89@gmail.com>
2018-09-18 13:01:22 -0600

    Quick updates

--- outcome_measurement/malaria/Gtm_bednets.R
+++ outcome_measurement/malaria/Gtm_bednets.R
@@ -1,3 +1,6 @@
+library(data.table)
+library(stringr)
+
 ml17 = read.csv("PCE/Outcome Measurement Data/MALARIA/Distribucion de MTILD/BD 2017/BD MTILD Escuintla P13/EntregaPIIs.csv")
 ml17d = read.csv("PCE/Outcome Measurement Data/MALARIA/Distribucion de MTILD/BD 2017/BD MTILD Escuintla P13/EntregaPIIsDetalle.csv")
 pabellones = data.table(merge(ml17d[,c("Pabellones", "CodBoleta")], ml17[,c("CodBoleta", "FechaEnt")], by="CodBoleta"))
