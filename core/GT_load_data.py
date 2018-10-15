15c888589f35677c3d1839f2f643baa5c00f4945
Guillermo <lgar89@gmail.com>
2018-10-15 09:46:34 -0600

    fix load data

--- core/GT_load_data.py
+++ core/GT_load_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-munisGT = pd.read_csv("../../Covariates and Other Data/Demographics/Guatemala_Municipios_IGN2017_worldpop2010-2012-2015.csv")
+munisGT = pd.read_csv("../../Covariates and Other Data/Demographics/Guatemala_Municipios_IGN2017_worldpop2010-2012-2015.csv", index_col = None)
 # There are two municipalities that are actually one. To handle this unique case I am making the
 # following groupby.
 munisGT = munisGT.groupby("COD_MUNI__").agg({
@@ -20,8 +20,13 @@ splitted_municipalities = pd.DataFrame(data = {
     "year_of_split": [2015,2015,2014,2011,2014,2014,2014]
 })
 
-munisGT.columns = ["municode", "name", "deptocode", "depto", "area", 
-                   "Poblacion2010", "Poblacion2012", "Poblacion2015"]
+munisGT.rename(columns = {
+    "NOMBRE__": "name",
+    "COD_DEPT__": "deptocode",
+    "DEPTO__": "depto",
+    "AREA_KM__": "area",
+    "COD_MUNI__": "municode"
+}, inplace = True)
 munisGT = munisGT.merge(splitted_municipalities, 
                         left_on = "municode", right_on = "new_code", how="outer")
 munisGT["parent_code"] = munisGT.apply(lambda x: x.municode if np.isnan(x.parent_code) else x.parent_code, axis="columns")
@@ -35,5 +40,6 @@ munisGT_2009 = munisGT.groupby("parent_code").agg({
     "Poblacion2012": "sum",
     "Poblacion2015": "sum"
 }).reset_index()
-munisGT_2009.columns = ["municode", "name", "deptocode", "depto", "area", 
-                   "Poblacion2010", "Poblacion2012", "Poblacion2015"]
+munisGT_2009.rename(columns = {
+    "parent_code": "municode"
+}, inplace = True)
